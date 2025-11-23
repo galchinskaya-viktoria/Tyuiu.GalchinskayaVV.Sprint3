a6821897b7b42db03a2ed0a4dbbb78b668375301
Tyuiu.GalchinskayaVV@outlook.com <yshumicu@list.ru>
2025-11-23 15:08:31 +0500

    Task0

---
+++ Tyuiu.GalchinskayaVV.Sprint3.Task0.V11.Lib/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint3.Task0.V11.Lib
+{
+    public class Class1
+    {
+
+    }
+}
---
+++ Tyuiu.GalchinskayaVV.Sprint3.Task0.V11.Test/Test1.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.GalchinskayaVV.Sprint3.Task0.V11.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
