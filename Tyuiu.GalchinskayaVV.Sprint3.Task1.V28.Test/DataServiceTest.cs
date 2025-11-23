ec100b0903389602a5a7ae8dc43e0faaa399f033
Zahar <83747187+ZaharrRR@users.noreply.github.com>
2025-11-23 15:21:09 +0500

    Task 1

---
+++ Tyuiu.GalchinskayaVV.Sprint3.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.GalchinskayaVV.Sprint3.Task1.V28.Lib;
+
+namespace Tyuiu.GalchinskayaVV.Sprint3.Task1.V28.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestSumSeries()
+        {
+            DataService ds = new DataService();
+
+            double a = 0.5;
+            double result = ds.GetSumSeries(a, 1, 18);
+
+            double expected = -0.186;
+
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
